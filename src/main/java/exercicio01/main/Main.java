f4839c2066462292f9c99bb837707fe07877fc4a
Ronan <ronan-com@hotmil.com>
2021-03-27 18:36:30 -0300

    Primeiro Commit

---
+++ src/main/java/exercicio01/main/Main.java
@@ -0,0 +1,80 @@
+    package exercicio01.main;
+
+    import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Main app = new Main ();
+            app.start();
+
+        }
+
+        private void start() {
+            
+            
+            int contagem =3;
+            int[] valores = new int[4];
+            int []retorno = obterValores(contagem,valores);  
+            calcularValores(retorno);
+            
+            
+            switch (retorno[i]) {
+            
+            case (retorno[i]>=50):
+            System.out.println("Valor muito alto");
+            break;
+            
+            
+            case (<=50): 
+                System.out.println("Valor normal");
+                break;
+                
+            }
+            }
+        
+    } 
+            
+        
+        private int [] obterValores(int contagem, int valores[]){
+            
+            Scanner ler = new Scanner(System.in);
+            int[] retorno  = new int [4];
+            for(int i=0; i<=contagem; i++) {
+                System.out.println("Digite um valor");
+                valores[i]= ler.nextInt();  
+                
+                retorno[i] = valores[i];
+            }
+                
+            return retorno;
+            
+        }
+        private void calcularValores(int retorno[]){
+            
+            int maior = 0;
+            int menor = 0;
+            int i=3;
+            
+            for (i=0; i<= retorno.length; i++) {
+                
+                if (retorno[i]> maior) {
+                    maior = retorno[i];
+                    
+                }
+                else((retorno[i] < maior)&(retorno[i] >= menor)){
+                    
+                    menor= retorno[i];
+                }
+                
+                System.out.println("O maior valor é: "+ maior);
+                System.out.println("O menor valor é: "+ menor);
+                
+            }
+            
+            
+            
+        
+     }
+ }
+        
